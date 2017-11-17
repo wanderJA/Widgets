e9586c9e3c95b73e67ca57d45445cd37c33e067f
wander <wangdou@gozap.com>
2017-11-17 18:05:27 +0800

    no

---
+++ app/src/main/java/com/wander/widgets/bean/RulerBean.kt
@@ -0,0 +1,15 @@
+package com.wander.widgets.bean
+
+/**
+ * @author wander
+ * @date 2017/11/17
+ *
+ */
+class RulerBean {
+    var value: Int = 0
+
+    constructor()
+    constructor(value: Int) {
+        this.value = value
+    }
+}
