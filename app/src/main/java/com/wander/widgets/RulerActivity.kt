16cbc1568ebd0541308a950c7b3d0e9de868be26
wander <wangdou@gozap.com>
2017-11-17 17:46:07 +0800

    Initial commit

---
+++ app/src/main/java/com/wander/widgets/RulerActivity.kt
@@ -0,0 +1,12 @@
+package com.wander.widgets
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RulerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ruler)
+    }
+}
